85cc7afb732d6a8a3e2ab2ed85bd4c860ccef7e2
Nikhil <nikhil@Johns-iMac.local>
2012-08-03 13:04:43 -0500

    correct through association typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
   has_many :addresses
-  has_many :orders, :through => :address
+  has_many :orders, :through => :addresses
   
   
   
